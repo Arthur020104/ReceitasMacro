0856e600c71d8c8adbefbc64d419099b56a4d173
Arthur <arthur020104@gmail.com>
2022-04-16 12:19:26 -0300

    trying to deploy on heruko

--- finalproject_cs50web/settings.py
+++ finalproject_cs50web/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for finalproject_cs50web project.
 
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,5 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
