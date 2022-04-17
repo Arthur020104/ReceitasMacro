2ddb6813a0ea635de14e964ef149ee5a9312620f
Arthur <arthur020104@gmail.com>
2022-04-16 23:12:12 -0300

    fazendo algumas atualizacoes no estilo e criando pagina minhas receitas

--- finalproject_cs50web/settings.py
+++ finalproject_cs50web/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-llunc&4$j=j%6hk6-thdzgf76t0ab@)wfjb^-8+s5(fy^)f)oi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['receitasmacro.herokuapp.com','127.0.0.1']
 
