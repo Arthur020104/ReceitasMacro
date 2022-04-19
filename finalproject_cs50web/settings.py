d30355629859ec39b4d34829b8d39296e5bb4c97
Arthur <arthur020104@gmail.com>
2022-04-18 22:31:21 -0300

    makingmigration and adding static_root again

--- finalproject_cs50web/settings.py
+++ finalproject_cs50web/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = '/static/'
 STATIC_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
